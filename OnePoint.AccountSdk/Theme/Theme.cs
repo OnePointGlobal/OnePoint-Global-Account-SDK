97b36c901ca115614fb439a9becc98a1b8f9019a
abhi-onepoint <abhi.patil@onepointglobal.com>
2017-11-23 15:18:28 +0530

    -Code insight & code quality analysis.

--- OnePoint.AccountSdk/Theme/Theme.cs
+++ OnePoint.AccountSdk/Theme/Theme.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 
 namespace OnePoint.AccountSdk.Theme
 {
     public class Theme
     {
-        public int ThemeTemplateID { get; set; }
+        public int ThemeTemplateId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public bool IsDeleted { get; set; }
         public string CreatedDate { get; set; }
         public string LastUpdatedDate { get; set; }
-        public int UserID { get; set; }
     }
 
     public class ThemeList
     {
         public object MediaUrl { get; set; }
         public int ThemeID { get; set; }
-        public int ThemeTemplateID { get; set; }
-        public int ThemeElementTypeID { get; set; }
+        public int ThemeTemplateId { get; set; }
+        public int ThemeElementTypeId { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
         public bool IsDeleted { get; set; }
@@ -30,14 +26,14 @@ namespace OnePoint.AccountSdk.Theme
         public string LastUpdatedDate { get; set; }
     }
 
-    public class RootObject
+    public class ThemeRoot
     {
         public List<Theme> Themes { get; set; }
         public string ErrorMessage { get; set; }
         public bool IsSuccess { get; set; }
     }
 
-    public class ThemeListRootObject
+    public class ThemeListRoot
     {
         public List<ThemeList> ThemeList { get; set; }
         public string ErrorMessage { get; set; }
--- OnePoint.AccountSdk/Theme/ThemeRoute.cs
+++ OnePoint.AccountSdk/Theme/ThemeRoute.cs
@@ -1,10 +1,7 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OnePoint.AccountSdk.Theme
@@ -12,87 +9,87 @@ namespace OnePoint.AccountSdk.Theme
     public class ThemeRoute
     {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        AdminRequestHandler requestHandler { get; set; }
+        private AdminRequestHandler RequestHandler { get; }
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private Result result = new Result();
+        private readonly Result _result = new Result();
 
         public ThemeRoute(AdminRequestHandler hanlder)
         {
-            this.requestHandler = hanlder;
+            RequestHandler = hanlder;
         }
 
-        public RootObject GetThemes(ThemeType type)
+        public ThemeRoot GetThemes(ThemeType type)
         {
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserTheme/GetThemes?type=" + (short)type, HttpMethod.Get, RouteStyle.Rpc, null);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserTheme/GetThemes?type=" + (short)type, HttpMethod.Get, RouteStyle.Rpc, null);
             x.Wait();
-            return x.Result.JsonToObject(new RootObject(), "Themes");
+            return x.Result.JsonToObject(new ThemeRoot(), "Themes");
         }
 
-        public ThemeListRootObject GetThemeDetails(long themeTemplateId)
+        public ThemeListRoot GetThemeDetails(long themeTemplateId)
         {
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserTheme/GetThemeDetail?themeTemplateId=" + themeTemplateId, HttpMethod.Get, RouteStyle.Rpc, null);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserTheme/GetThemeDetail?themeTemplateId=" + themeTemplateId, HttpMethod.Get, RouteStyle.Rpc, null);
             x.Wait();
-            return x.Result.JsonToObject(new ThemeListRootObject(), "ThemeList");
+            return x.Result.JsonToObject(new ThemeListRoot(), "ThemeList");
         }
 
-        public RootObject DuplicateTheme(long themeTemplateId)
+        public ThemeRoot DuplicateTheme(long themeTemplateId)
         {
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserTheme/CopyTheme?themeTemplateId=" + themeTemplateId, HttpMethod.Get, RouteStyle.Rpc, null);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserTheme/CopyTheme?themeTemplateId=" + themeTemplateId, HttpMethod.Get, RouteStyle.Rpc, null);
             x.Wait();
-            return x.Result.JsonToObject(new RootObject(), "Themes");
+            return x.Result.JsonToObject(new ThemeRoot(), "Themes");
         }
 
-        public RootObject DeleteThemes(List<long> deleteThemeTemplateIds)
+        public ThemeRoot DeleteThemes(List<long> deleteThemeTemplateIds)
         {
             var requestArg = JsonConvert.SerializeObject(new { deleteThemeTemplateIds = string.Join(",", deleteThemeTemplateIds) });
             requestArg = JsonConvert.SerializeObject(new { Data = requestArg });
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserTheme/DeleteTheme", HttpMethod.Delete, RouteStyle.Rpc, requestArg);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserTheme/DeleteTheme", HttpMethod.Delete, RouteStyle.Rpc, requestArg);
             x.Wait();
 
-            return x.Result.JsonToObject(new RootObject(), "Themes");
+            return x.Result.JsonToObject(new ThemeRoot(), "Themes");
         }
 
-        public RootObject AddTheme(string name, string description, ThemeType type)
+        public ThemeRoot AddTheme(string name, string description, ThemeType type)
         {
             if (string.IsNullOrEmpty(name))
             {
-                return result.ErrorToObject(new RootObject(), "Invalid parameter(s)");
+                return _result.ErrorToObject(new ThemeRoot(), "Invalid parameter(s)");
             }
 
             var requestArg = JsonConvert.SerializeObject(new { Name = name, Description = description, Type = (short)type });
             requestArg = JsonConvert.SerializeObject(new { Data = requestArg });
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserTheme/AddTheme?Name=" + name + "&Description=" + description, HttpMethod.Post, RouteStyle.Rpc, requestArg);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserTheme/AddTheme?Name=" + name + "&Description=" + description, HttpMethod.Post, RouteStyle.Rpc, requestArg);
             x.Wait();
 
-            return x.Result.JsonToObject(new RootObject(), "Themes");
+            return x.Result.JsonToObject(new ThemeRoot(), "Themes");
         }
 
 
-        public ThemeListRootObject SaveAppTheme(long themeTemplateID, string actionButton, string logoText, string linksColor)
+        public ThemeListRoot SaveAppTheme(long themeTemplateID, string actionButton, string logoText, string linksColor)
         {
             if (themeTemplateID <= 0 || string.IsNullOrEmpty(actionButton) || string.IsNullOrEmpty(logoText) || string.IsNullOrEmpty(linksColor))
             {
-                return result.ErrorToObject(new ThemeListRootObject(), "Invalid parameter(s)");
+                return _result.ErrorToObject(new ThemeListRoot(), "Invalid parameter(s)");
             }
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserTheme/SaveAppTheme?themeTemplateId=" + themeTemplateID + "&Actionbtn=" + actionButton + "&Logotext=" + logoText + "&Linkscolor=" + linksColor, HttpMethod.Post, RouteStyle.Rpc, null);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserTheme/SaveAppTheme?themeTemplateId=" + themeTemplateID + "&Actionbtn=" + actionButton + "&Logotext=" + logoText + "&Linkscolor=" + linksColor, HttpMethod.Post, RouteStyle.Rpc, null);
             x.Wait();
-            return x.Result.JsonToObject(new ThemeListRootObject(), "ThemeList");
+            return x.Result.JsonToObject(new ThemeListRoot(), "ThemeList");
 
         }
 
-        public ThemeListRootObject SaveAdminTheme(long themeTemplateID, string brandColor, string secondaryColor, string logoText)
+        public ThemeListRoot SaveAdminTheme(long themeTemplateID, string brandColor, string secondaryColor, string logoText)
         {
             if (themeTemplateID <= 0 || string.IsNullOrEmpty(brandColor) || string.IsNullOrEmpty(logoText) || string.IsNullOrEmpty(secondaryColor))
             {
-                return result.ErrorToObject(new ThemeListRootObject(), "Invalid parameter(s)");
+                return _result.ErrorToObject(new ThemeListRoot(), "Invalid parameter(s)");
             }
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserTheme/SaveAdminTheme?themeTemplateId=" + themeTemplateID + "&Brandcolor=" + brandColor + "&Secondarycolor=" + secondaryColor + "&Logotext=" + logoText, HttpMethod.Post, RouteStyle.Rpc, null);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserTheme/SaveAdminTheme?themeTemplateId=" + themeTemplateID + "&Brandcolor=" + brandColor + "&Secondarycolor=" + secondaryColor + "&Logotext=" + logoText, HttpMethod.Post, RouteStyle.Rpc, null);
             x.Wait();
-            return x.Result.JsonToObject(new ThemeListRootObject(), "ThemeList");
+            return x.Result.JsonToObject(new ThemeListRoot(), "ThemeList");
 
         }
     }
