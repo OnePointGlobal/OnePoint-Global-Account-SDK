043fc5d9246bcf1911416ab8fcd1292f7ebb3c90
abhi-onepoint <abhi.patil@onepointglobal.com>
2017-11-28 14:37:29 +0530

    Code insight & quality analysis.

--- OnePoint.AccountSdk/Email/Email.cs
+++ OnePoint.AccountSdk/Email/Email.cs
@@ -1,26 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace OnePoint.AccountSdk.Email
 {
     public class EmailTemplate
     {
-        public int EmailTemplateID { get; set; }
+        public int EmailTemplateId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Subject { get; set; }
         public string EmailContent { get; set; }
-        public int UserID { get; set; }
+        public int EmailType { get; set; }
         public bool IsDeleted { get; set; }
         public string CreatedDate { get; set; }
         public string LastUpdatedDate { get; set; }
-        public string Subject { get; set; }
-        public int EmailType { get; set; }
-    }
+   }
 
-    public class RootObject
+    public class EmailRoot
     {
         public List<EmailTemplate> EmailTemplates { get; set; }
         public string ErrorMessage { get; set; }
--- OnePoint.AccountSdk/Email/EmailRoute.cs
+++ OnePoint.AccountSdk/Email/EmailRoute.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OnePoint.AccountSdk.Email
@@ -12,98 +10,98 @@ namespace OnePoint.AccountSdk.Email
     public class EmailRoute
     {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        AdminRequestHandler requestHandler { get; set; }
+        private AdminRequestHandler RequestHandler { get; }
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private Result result = new Result();
+        private readonly Result _result = new Result();
 
         public EmailRoute(AdminRequestHandler hanlder)
         {
-            this.requestHandler = hanlder;
+            RequestHandler = hanlder;
         }
 
-        public RootObject GetEmailTemplates()
+        public EmailRoot GetEmailTemplates()
         {
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserEmail/GetTemplates", HttpMethod.Get, RouteStyle.Rpc, null);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserEmail/GetTemplates", HttpMethod.Get, RouteStyle.Rpc, null);
             x.Wait();
-            return x.Result.JsonToObject(new RootObject(), "EmailTemplates");
+            return x.Result.JsonToObject(new EmailRoot(), "EmailTemplates");
         }
 
-        public RootObject AddEmailTemplate(string name, string description, string subject, string emailContent)
+        public EmailRoot AddEmailTemplate(string name, string description, string subject, string emailContent)
         {
             if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(description) || string.IsNullOrEmpty(subject) || string.IsNullOrEmpty(emailContent))
             {
-                return result.ErrorToObject(new RootObject(), "Invalid parameter(s)");
+                return _result.ErrorToObject(new EmailRoot(), "Invalid parameter(s)");
             }
 
             var requestArg = JsonConvert.SerializeObject(new { Name = name, Description = description, EmailContent = emailContent, Subject = subject });
             requestArg = JsonConvert.SerializeObject(new { Data = requestArg });
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserEmail/AddTemplate", HttpMethod.Post, RouteStyle.Rpc, requestArg);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserEmail/AddTemplate", HttpMethod.Post, RouteStyle.Rpc, requestArg);
             x.Wait();
 
-            return x.Result.JsonToObject(new RootObject(), "EmailTemplates");
+            return x.Result.JsonToObject(new EmailRoot(), "EmailTemplates");
         }
 
-        public RootObject CopyEmailTemplate(long emailTemplateId)
+        public EmailRoot CopyEmailTemplate(long emailTemplateId)
         {
             if (emailTemplateId < 1)
             {
-                return result.ErrorToObject(new RootObject(), "Invalid parameter(s)");
+                return _result.ErrorToObject(new EmailRoot(), "Invalid parameter(s)");
             }
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserEmail/CopyTemplate?emailTemplateId=" + emailTemplateId, HttpMethod.Get, RouteStyle.Rpc, null);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserEmail/CopyTemplate?emailTemplateId=" + emailTemplateId, HttpMethod.Get, RouteStyle.Rpc, null);
             x.Wait();
-            return x.Result.JsonToObject(new RootObject(), "EmailTemplates");
+            return x.Result.JsonToObject(new EmailRoot(), "EmailTemplates");
         }
 
-        public RootObject DeleteEmailTemplates(List<long> emailTemplateIds)
+        public EmailRoot DeleteEmailTemplates(List<long> emailTemplateIds)
         {
             if (emailTemplateIds.Count < 1)
             {
-                return result.ErrorToObject(new RootObject(), "Invalid parameter(s)"); ;
+                return _result.ErrorToObject(new EmailRoot(), "Invalid parameter(s)"); ;
             }
 
             var requestArg = JsonConvert.SerializeObject(new { TemplateIDs = String.Join(",", emailTemplateIds) });
             requestArg = JsonConvert.SerializeObject(new { Data = requestArg });
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserEmail/DeleteTemplates", HttpMethod.Delete, RouteStyle.Rpc, requestArg);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserEmail/DeleteTemplates", HttpMethod.Delete, RouteStyle.Rpc, requestArg);
             x.Wait();
 
-            return x.Result.JsonToObject(new RootObject(), "EmailTemplates");
+            return x.Result.JsonToObject(new EmailRoot(), "EmailTemplates");
         }
 
-        public RootObject UpdateEmailTemplate(long emailTemplateID, string name, string description, string subject, string emailContent)
+        public EmailRoot UpdateEmailTemplate(long emailTemplateId, string name, string description, string subject, string emailContent)
         {
             if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(description) || string.IsNullOrEmpty(subject) || string.IsNullOrEmpty(emailContent))
             {
-                return result.ErrorToObject(new RootObject(), "Invalid parameter(s)");
+                return _result.ErrorToObject(new EmailRoot(), "Invalid parameter(s)");
             }
 
 
-            var requestArg = JsonConvert.SerializeObject(new { EmailTemplateID = emailTemplateID, Name = name, EditDescription = description, EmailContent = emailContent, Subject = subject });
+            var requestArg = JsonConvert.SerializeObject(new { EmailTemplateID = emailTemplateId, Name = name, EditDescription = description, EmailContent = emailContent, Subject = subject });
             requestArg = JsonConvert.SerializeObject(new { Data = requestArg });
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserEmail/UpdateTemplate", HttpMethod.Put, RouteStyle.Rpc, requestArg);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserEmail/UpdateTemplate", HttpMethod.Put, RouteStyle.Rpc, requestArg);
             x.Wait();
 
-            return x.Result.JsonToObject(new RootObject(), "EmailTemplates");
+            return x.Result.JsonToObject(new EmailRoot(), "EmailTemplates");
         }
 
-        public RootObject SendEmail(string emailIds, long surveyId, EmailType emailType, long emailServerId, string subject, string emailContent)
+        public EmailRoot SendEmail(string emailIds, long surveyId, EmailType emailType, long emailServerId, string subject, string emailContent)
         {
             if (string.IsNullOrEmpty(emailIds) || string.IsNullOrEmpty(subject) || string.IsNullOrEmpty(emailContent) || emailServerId < 1)
             {
-                return result.ErrorToObject(new RootObject(), "Invalid parameter(s)");
+                return _result.ErrorToObject(new EmailRoot(), "Invalid parameter(s)");
             }
 
             var requestArg = JsonConvert.SerializeObject(new { SurveyID = surveyId, EmailTemplateID = 0, EmailServerId = emailServerId, EmailAddress = emailIds, EmailType = emailType, Subject = subject, EmailBody = emailContent });
             requestArg = JsonConvert.SerializeObject(new { Data = requestArg });
 
-            Task<Result> x = this.requestHandler.SendRequestAsync(string.Empty, "api/UserEmail/SendEmail", HttpMethod.Post, RouteStyle.Rpc, requestArg);
+            Task<Result> x = RequestHandler.SendRequestAsync(string.Empty, "api/UserEmail/SendEmail", HttpMethod.Post, RouteStyle.Rpc, requestArg);
             x.Wait();
 
-            return x.Result.JsonToObject(new RootObject(), "EmailTemplates");
+            return x.Result.JsonToObject(new EmailRoot(), "EmailTemplates");
         }
     }
 }
