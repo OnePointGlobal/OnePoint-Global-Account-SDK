8cc972eba980d16e07ce2efb9c1298b5cf015693
abhi-onepoint <abhi.patil@onepointglobal.com>
2018-01-16 16:43:21 +0530

    New module for survey notification 1. NotificationRoute.cs - Manages the code to do CRUD operation on survey sms, app and email notification data.

---
+++ OnePoint.AccountSdk/Schedule/NotificationRoute.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace OnePoint.AccountSdk.Schedule
+{
+    public class NotificationRoute
+    {
+        /// <summary>
+        /// The result.
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly Result _result = new Result();
+
+        /// <summary>
+        /// Gets or sets the request handler.
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private AdminRequestHandler RequestHandler { get; }
+
+        public NotificationRoute(AdminRequestHandler hanlder)
+        {
+            RequestHandler = hanlder;
+        }
+    }
+}
